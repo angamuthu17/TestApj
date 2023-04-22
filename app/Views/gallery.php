c41c1d0b3c9e8ab15c51f3b88e45426bc159ecfc
V Vignesh Arumugam <vignesharumugam.v@hotmail.com>
2023-04-22 17:08:21 +0530

    Your Message

---
+++ app/Views/gallery.php
@@ -0,0 +1,103 @@
+<style>
+        .container .gallery a img {
+            float: left;
+            width: 25%;
+            height: auto;
+            border: 2px solid #fff;
+            -webkit-transition: -webkit-transform .15s ease;
+            -moz-transition: -moz-transform .15s ease;
+            -o-transition: -o-transform .15s ease;
+            -ms-transition: -ms-transform .15s ease;
+            transition: transform .15s ease;
+            position: relative;
+        }
+
+        .clear {
+            clear: both;
+        }
+
+        a {
+            color: #009688;
+            text-decoration: none;
+        }
+
+        a:hover {
+            color: #01695f;
+            text-decoration: none;
+        }
+
+        .thumb-img {
+            border: 1px solid #ddd; /* Gray border */
+            border-radius: 4px;  /* Rounded border */
+            padding: 5px; /* Some padding */
+            max-width:150px;
+            max-height:100px;
+            width: auto;
+            height: auto;
+        }
+    </style>
+
+<main role="main">
+  <div class="container mt-5 mb-5">
+
+  <div id="whoWeAre" class="jumbotron jumbotron-fluid text-border-big">
+      <div class="container">
+          <h3>Gallery</h3>
+      </div>
+  </div>
+  
+  <div class="container">
+    <h4 class="mt-4">Support for women entrepreneurs</h4>
+    <p class="lead mt-2">Coordinated and Funded to support road side small business women entrepreneurs - April 2023</p>
+    <div class="lightbox-gallery">
+        <a href="assets/images/pic15.jpeg" class="big"><img class="thumb-img" src="assets/images/pic15.jpeg" alt="" title="" /></a>
+        <a href="assets/images/pic16.jpeg" class="big"><img class="thumb-img" src="assets/images/pic16.jpeg" alt="" title="" /></a>
+        <a href="assets/images/pic17.jpeg" class="big"><img class="thumb-img" src="assets/images/pic17.jpeg" alt="" title="" /></a>
+        <a href="assets/images/pic18.jpeg" class="big"><img class="thumb-img" src="assets/images/pic18.jpeg" alt="" title="" /></a>
+        <a href="assets/images/pic19.jpeg" class="big"><img class="thumb-img" src="assets/images/pic19.jpeg" alt="" title="" /></a>
+        <a href="assets/images/pic20.jpeg" class="big"><img class="thumb-img" src="assets/images/pic20.jpeg" alt="" title="" /></a>
+        <a href="assets/images/pic21.jpeg" class="big"><img class="thumb-img" src="assets/images/pic21.jpeg" alt="" title="" /></a>
+        <a href="assets/images/pic22.jpeg" class="big"><img class="thumb-img" src="assets/images/pic22.jpeg" alt="" title="" /></a>
+        <a href="assets/images/pic23.jpeg" class="big"><img class="thumb-img" src="assets/images/pic23.jpeg" alt="" title="" /></a>
+        <a href="assets/images/pic24.jpeg" class="big"><img class="thumb-img" src="assets/images/pic24.jpeg" alt="" title="" /></a>
+        <a href="assets/images/pic25.jpeg" class="big"><img class="thumb-img" src="assets/images/pic25.jpeg" alt="" title="" /></a>
+        <div class="clear"></div>
+    </div>
+    <hr class="featurette-divider">
+    <h4 class="">Independence Day</h4>
+    <p class="lead">Independence day celebration with school children.</p>
+    <div class="lightbox-gallery1">
+        <a href="assets/images/pic6.jpeg" class="big"><img class="thumb-img" src="assets/images/pic6.jpeg" alt="" title="" /></a>
+        <a href="assets/images/pic4.jpeg" class="big"><img class="thumb-img" src="assets/images/pic4.jpeg" alt="" title="" /></a>
+        <div class="clear"></div>
+    </div>
+    <hr class="featurette-divider">
+    <h4 class="">Techie to Teach</h4>
+    <p class="lead">Our Volunteer's teach our children every saturday.</p>
+    <div class="lightbox-gallery2">
+        <a href="assets/images/pic3.jpeg" class="big"><img class="thumb-img" src="assets/images/pic3.jpeg" alt="" title="" /></a>
+        <a href="assets/images/pic1.jpeg" class="big"><img class="thumb-img" src="assets/images/pic1.jpeg" alt="" title="" /></a>
+        <div class="clear"></div>
+    </div>
+    <hr class="featurette-divider">
+    <h4 class=""> Help in Need (Medical/Education)</h4>
+    <p class="lead">Helping in Medical Emargency and Supporting children eduction without breaking.</p>
+    <div class="lightbox-gallery3">
+        <a href="assets/images/pic2.jpeg" class="big"><img class="thumb-img" src="assets/images/pic2.jpeg" alt="" title="" /></a>
+        <a href="assets/images/pic8.jpeg" class="big"><img class="thumb-img" src="assets/images/pic8.jpeg" alt="" title="" /></a>
+        <div class="clear"></div>
+    </div>
+    <hr class="featurette-divider">
+    <h4 >Sharing and Caring</h4>
+    <p class="lead">Our member is distributing food to elders in the senior care center.</p>
+    <div class="lightbox-gallery4">
+        <a href="assets/images/pic5.jpeg" class="big"><img class="thumb-img" src="assets/images/pic5.jpeg" alt="" title="" /></a>
+        <a href="assets/images/pic11.jpeg" class="big"><img class="thumb-img" src="assets/images/pic11.jpeg" alt="" title="" /></a>
+        <a href="assets/images/pic12.jpeg" class="big"><img class="thumb-img" src="assets/images/pic12.jpeg" alt="" title="" /></a>
+        <div class="clear"></div>
+    </div>
+    
+</div>
+
+  </div>
+</main>
